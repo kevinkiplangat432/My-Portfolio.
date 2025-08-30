bbf8f71f1c713270e38b2677c590f41df618f023
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-08-31 02:01:07 +0300

    adds javascript to the document

---
+++ javascript/homepage.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("index");
+
+    button.addEventListener("click", function() {
+        window.location.href = "/index.html"; // Redirect to index.html
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button1");
+
+    button.addEventListener("click", function() {
+        window.location.href = "homepage.html"; // Redirect to index.html
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button1");
+
+    button.addEventListener("click", function() {
+        window.location.href = "homepage.html"; // Redirect to index.html
+    });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button1");
+
+    button.addEventListener("click", function() {
+        window.location.href = "homepage.html"; // Redirect to index.html
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button1");
+
+    button.addEventListener("click", function() {
+        window.location.href = "homepage.html"; // Redirect to index.html
+    });
+});
