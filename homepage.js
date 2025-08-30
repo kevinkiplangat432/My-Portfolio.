fe29b5547b35d664b5ca3516582d84e0c3644978
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-08-31 02:18:40 +0300

    adds flowless file path

--- homepage.js
+++ homepage.js
@@ -1,47 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button1");
+
+    button.addEventListener("click", function() {
+        window.location.href = "homepage.html"; // Redirect to homepage.html
+    });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("index");
 
     button.addEventListener("click", function() {
-        window.location.href = "/index.html"; // Redirect to index.html
+        window.location.href = "index.html"; // Redirect to index.html
     });
 });
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("button1");
+    const button = document.getElementById("about");
 
     button.addEventListener("click", function() {
-        window.location.href = "homepage.html"; // Redirect to index.html
-    });
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("button1");
-
-    button.addEventListener("click", function() {
-        window.location.href = "homepage.html"; // Redirect to index.html
-    });
-});
-
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("button1");
-
-    button.addEventListener("click", function() {
-        window.location.href = "homepage.html"; // Redirect to index.html
+        window.location.href = "about.html"; // Redirect to about.html
     });
 });
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("button1");
+    const button = document.getElementById("skills");
 
     button.addEventListener("click", function() {
-        window.location.href = "homepage.html"; // Redirect to index.html
+        window.location.href = "skills.html"; // Redirect to skills.html
+    });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("project");
+
+    button.addEventListener("click", function() {
+        window.location.href = "projects.html"; // Redirect to projects.html
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("contact");
+
+    button.addEventListener("click", function() {
+        window.location.href = "contact.html"; // Redirect to contact.html
     });
 });
--- landingpage.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("button1");
-
-    button.addEventListener("click", function() {
-        window.location.href = "homepage.html"; // Redirect to index.html
-    });
-});
